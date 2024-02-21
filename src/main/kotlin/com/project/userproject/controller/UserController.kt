ccd479958e9ba8b3c129067423d6e2f795965748
AthosDeveloper <athos.silva@zup.com.br>
2024-02-21 12:07:53 -0300

    feat: add getUserById

--- src/main/kotlin/com/project/userproject/controller/UserController.kt
+++ src/main/kotlin/com/project/userproject/controller/UserController.kt
@@ -4,6 +4,7 @@ import com.project.userproject.domain.model.User
 import com.project.userproject.service.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,5 +13,9 @@ import org.springframework.web.bind.annotation.RestController
 class UserController (private  val userService: UserService) {
 @GetMapping
 fun listUsers(): ResponseEntity<List<User>> = ResponseEntity.ok(userService.findAll())
+@GetMapping("/{id}")
+fun getUserById(@PathVariable id: Long): ResponseEntity<User> =
+        userService.findById(id)?.let { ResponseEntity.ok(it) } ?: ResponseEntity.notFound().build()
+
 
 }
