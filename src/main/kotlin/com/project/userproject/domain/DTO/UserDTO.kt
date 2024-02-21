c0587c9e6c8aec3a8497e05c7014b653b4741d24
AthosDeveloper <athos.silva@zup.com.br>
2024-02-21 16:52:23 -0300

    feat: adjusts userService and userController to use DTO

--- src/main/kotlin/com/project/userproject/domain/DTO/UserDTO.kt
+++ src/main/kotlin/com/project/userproject/domain/DTO/UserDTO.kt
@@ -1,6 +1,6 @@
 package com.project.userproject.domain.DTO
 
-class UserDTO(var id: Long? = null,
+data class UserDTO(var id: Long? = null,
        var name: String,
         var email: String
         )
