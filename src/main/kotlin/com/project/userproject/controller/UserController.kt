b77192ee6a938f496ed9ad3464b1297ef2ae1f68
AthosDeveloper <athos.silva@zup.com.br>
2024-02-21 13:37:52 -0300

    feat: add updateUser

--- src/main/kotlin/com/project/userproject/controller/UserController.kt
+++ src/main/kotlin/com/project/userproject/controller/UserController.kt
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -20,5 +21,11 @@ fun getUserById(@PathVariable id: Long): ResponseEntity<User> =
         userService.findById(id)?.let { ResponseEntity.ok(it) } ?: ResponseEntity.notFound().build()
 @PostMapping
 fun createUser(@RequestBody user: User): ResponseEntity<User> = ResponseEntity.ok(userService.save(user))
+@PutMapping("@{id}")
+fun updateUser(@PathVariable id: Long, @RequestBody user: User): ResponseEntity<User> =
+        userService.findById(id)?.let {
+            val updatedUser = it.copy(name = user.name, email = user.email)
+            ResponseEntity.ok(userService.save(updatedUser))
+        } ?:ResponseEntity.notFound().build()
 
 }
