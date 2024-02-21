9df78248d57da154bcdae4e01d1ba14485ac84ed
AthosDeveloper <athos.silva@zup.com.br>
2024-02-21 13:19:19 -0300

    feat: add createUser

--- src/main/kotlin/com/project/userproject/controller/UserController.kt
+++ src/main/kotlin/com/project/userproject/controller/UserController.kt
@@ -5,6 +5,8 @@ import com.project.userproject.service.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,6 +18,7 @@ fun listUsers(): ResponseEntity<List<User>> = ResponseEntity.ok(userService.find
 @GetMapping("/{id}")
 fun getUserById(@PathVariable id: Long): ResponseEntity<User> =
         userService.findById(id)?.let { ResponseEntity.ok(it) } ?: ResponseEntity.notFound().build()
-
+@PostMapping
+fun createUser(@RequestBody user: User): ResponseEntity<User> = ResponseEntity.ok(userService.save(user))
 
 }
